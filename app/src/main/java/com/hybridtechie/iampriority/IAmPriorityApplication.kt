8cd03fd7469653e9c66a1b67edaf6489e24c3637
Nithin James <ping.nith@gmail.com>
2019-05-17 14:54:20 +1000

    Add Notification

---
+++ app/src/main/java/com/hybridtechie/iampriority/IAmPriorityApplication.kt
@@ -0,0 +1,27 @@
+package com.hybridtechie.iampriority
+
+import android.app.Application
+import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+
+
+/**
+ * Created by HyBr!dT3cH!3 on 17/05/2019..
+ */
+class IAmPriorityApplication : Application() {
+
+    private lateinit var mFirebaseAnalytics: FirebaseAnalytics
+
+    override fun onCreate() {
+        super.onCreate()
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+    }
+
+    fun uploadEventToAnlytics(itemId: String, name: String, type: String) {
+        val bundle = Bundle()
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, itemId)
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, name)
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, type)
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
+    }
+}
