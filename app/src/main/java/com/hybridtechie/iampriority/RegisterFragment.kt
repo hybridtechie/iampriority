9cd7338f96132e47b4bba83572a4df5242dbc282
Nithin James <ping.nith@gmail.com>
2019-05-31 10:09:22 +1000

    Register Page

--- app/src/main/java/com/hybridtechie/iampriority/RegisterFragment.kt
+++ app/src/main/java/com/hybridtechie/iampriority/RegisterFragment.kt
@@ -1,10 +1,12 @@
 package com.hybridtechie.iampriority
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputEditText
 
 class RegisterFragment : Fragment() {
 
@@ -13,7 +15,51 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        val view = inflater.inflate(R.layout.fragment_register, container, false)
+
+        val countryChooser = view.findViewById<TextInputEditText>(R.id.country_chooser)
+        countryChooser.setOnClickListener { v -> showCountryDialog(v) }
+
+        val stateChooser = view.findViewById<TextInputEditText>(R.id.state_chooser)
+        stateChooser.setOnClickListener { v -> showStateDialog(v) }
+
+        val districtChooser = view.findViewById<TextInputEditText>(R.id.district_chooser)
+        districtChooser.setOnClickListener { v -> showDistrictDialog(v) }
+
+        return view
+    }
+
+    private fun showCountryDialog(v: View) {
+        val array = arrayOf("United State", "Germany", "United Kingdom", "Australia")
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Country")
+        builder.setSingleChoiceItems(array, -1) { dialogInterface, i ->
+            (v as TextInputEditText).setText(array[i])
+            dialogInterface.dismiss()
+        }
+        builder.show()
+    }
+
+    private fun showStateDialog(v: View) {
+        val array = arrayOf("Kerala", "Tamil Nadu", "Andra Pradesh", "Karnataka")
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("State")
+        builder.setSingleChoiceItems(array, -1) { dialogInterface, i ->
+            (v as TextInputEditText).setText(array[i])
+            dialogInterface.dismiss()
+        }
+        builder.show()
+    }
+
+    private fun showDistrictDialog(v: View) {
+        val array = arrayOf("Kottayam", "Thiruvananthapuram", "Kannur", "Pathanamthitta")
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("District")
+        builder.setSingleChoiceItems(array, -1) { dialogInterface, i ->
+            (v as TextInputEditText).setText(array[i])
+            dialogInterface.dismiss()
+        }
+        builder.show()
     }
 
     companion object {
